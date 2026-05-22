123856cd2d04c86baff4c437fd17514c31cbdd58
Ubuntu <ubuntu@localhost.localdomain>
2026-05-22 19:28:26 +0800

    clean: fix directory structure, all files under tical_code/guardian/

--- integration/worker_loop_adapter.py
+++
@@ -1,6 +0,0 @@
-
-from tical_code.guardian.signal_collector import SignalCollector
-from tical_code.guardian.state_classifier import StateClassifier
-def guardian_patrol():
-    sig=SignalCollector().collect()
-    return StateClassifier().classify(sig)
